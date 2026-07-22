fe5d194ce27715570cc50966c06d9f0cd8bb3156
tajulahasanath <tajulahasanath21@gmail.com>
2026-07-22 19:46:25 +0530

    Update auth.tsx

--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -76,9 +76,10 @@ queryParams: { prompt: "select_account" },
     setLoading(true);
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
-      options: {
-        redirectTo: `${window.location.origin}/dashboard`,
-      },
+      redirectTo: `${window.location.origin}/auth`,
+queryParams: {
+  prompt: "select_account",
+},
     });
     if (error) {
       setLoading(false);
